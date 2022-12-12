ce3defb7f1531cb3f43509abf04e262886f270b1
Thinking^loper <96408793+JaeWonLee3003@users.noreply.github.com>
2022-12-12 16:53:26 +0900

    2022_12_12

--- BaekjoonTest/Program.cs
+++ BaekjoonTest/Program.cs
@@ -5,26 +5,26 @@ public class Program
 
     public static void Main()
     {
-        // 첫째 줄에 연도가 주어진다. 연도는 1보다 크거나 같고, 4000보다 작거나 같은 자연수이다.
+        string[] Time = Console.ReadLine().Split();
+        int H = int.Parse(Time[0]);
+        int M = int.Parse(Time[1]);
+        if (H < 0 || H > 24 || M < 0 || M > 59) return;
 
-        int Year = int.Parse(Console.ReadLine());
 
-        if ( Year >= 1 || Year <= 4000) 
-
-        // 첫째 줄에 윤년이면 1, 아니면 0을 출력한다.
-        // What is 윤년?
-        // 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
-
-        // if 윤년인지 아닌지 판단문
-
-        if( Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0 )
+        if (M < 45) // 대충 시간이 0보다 작을때 23으로 해주고 분이 0보다 작을때 +60을 해주면되느데
         {
-            Console.WriteLine(1);
-        }
+            H--;
+            M += 15;
+            if (H < 0)
+            {
+                H = 23;
+            }
+        }      
         else
         {
-            Console.WriteLine(0);
+            M -= 45;
         }
+        Console.WriteLine(H + " " + M);
 
     }
 }
